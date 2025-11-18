2ce04cdc591be40c8f481a657624b8fe9577363f
Héctor Leoni <hectorlima1992@gmail.com>
2025-11-18 11:03:14 -0300

    incluir bd

---
+++ todo_db.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE todo_db;
+
+use todo_db;
+
+CREATE TABLE tasks (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  status ENUM('pending', 'completed') DEFAULT 'pending',
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
